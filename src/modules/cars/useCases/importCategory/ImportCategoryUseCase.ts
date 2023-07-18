98ff6c1622f1d9d36edc59647a09da9621f110e6
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-07-17 21:16:45 -0300

    feat: add stream and csv parse

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,15 @@
+import csvParse from "csv-parse";
 import fileSystem from "fs";
 
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
 
     const stream = fileSystem.createReadStream(file.path)
-    stream.pipe()
+    const parseFile = csvParse.parse()
+    stream.pipe(parseFile)
+    parseFile.on("data", async(line) => {
+      console.log(line) 
+    })
   }
 }
 export { ImportCategoryUseCase };
