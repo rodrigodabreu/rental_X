18e474d05d94fd7b27d9403f4bfa84a548d9a143
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-08-01 20:15:48 -0300

    feat: add feature to remove tmp file after persist data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -26,6 +26,7 @@ class ImportCategoryUseCase {
       categories.push({name, description})
     })
     .on("end", () => {
+      fileSystem.promises.unlink(file.path)
       resolve(categories)
     })
     .on("error", (err) => {
